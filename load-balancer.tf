d8b2d14f85dab1619bcef48db6e6f33c7d6a9535
JohnC-92 <chongxz@solab.me.ntu.edu.tw>
2020-12-09 10:59:37 +0800

    changed load balancer from http to https, added http to https url map

--- load-balancer.tf
+++ load-balancer.tf
@@ -7,47 +7,83 @@ resource "google_compute_global_address" "default-lb" {
   address_type = "EXTERNAL" 
 }
 
-// Global forwarding rule to forward traffic directed at port 80 of created ip address to target our HTTP proxy
-resource "google_compute_global_forwarding_rule" "default-forward-rule" {
-  name = "${var.project_name}-port-80"
+// ----------------------------------- HTTPS Load Balancer Settings -----------------------------------
+// Global forwarding rule to forward traffic directed at port 443 of created ip address to target our HTTPS proxy
+resource "google_compute_global_forwarding_rule" "default-https-forward-rule" {
+  name = "${var.project_name}-port-443"
   ip_address = google_compute_global_address.default-lb.address
-  port_range = "80"
-  target = google_compute_target_http_proxy.default.id
+  port_range = "443"
+  target = google_compute_target_https_proxy.default-https.id
 }
 
-// Target http proxy uses URL map to send traffic to backend
-resource "google_compute_target_http_proxy" "default" {
+// Target https proxy uses URL map to send traffic to backend
+resource "google_compute_target_https_proxy" "default-https" {
   name = var.project_name
-  url_map = google_compute_url_map.default.id
+  url_map = google_compute_url_map.default-https.id
+  ssl_certificates = [google_compute_ssl_certificate.default-https.id]
 }
 
-resource "google_compute_url_map" "default" {
-  name = var.project_name
-  default_service = google_compute_backend_service.default.id
+resource "google_compute_ssl_certificate" "default-https" {
+  name        = "${var.project_name}-certificate"
+  private_key = file(var.https_private_key)
+  certificate = file(var.https_certificate)
+}
+
+resource "google_compute_url_map" "default-https" {
+  name = "${var.project_name}-https"
+  default_service = google_compute_backend_service.default-https-lb.id
 }
 
 // Backend service consists of one or more instance group which are possible destinations for traffic from target proxy
-resource "google_compute_backend_service" "default" {
-  name = "${var.project_name}-backend-service"
-  protocol = "HTTP"
+resource "google_compute_backend_service" "default-https-lb" {
+  name = "${var.project_name}-https-backend-service"
+  protocol = "HTTPS"
   port_name = var.project_name
-  timeout_sec = 10
+  timeout_sec = 30
   backend {
     group = google_compute_instance_group.default-instance-group.id
   }
 
-  health_checks= [google_compute_health_check.http-health-check.id]
+  health_checks= [google_compute_health_check.https-health-check.id]
 }
 
 // Health checks are used by the load balancer to determine which instance can handle traffic
-resource "google_compute_health_check" "http-health-check" {
-  name = "http-health-check"
+resource "google_compute_health_check" "https-health-check" {
+  name = "https-health-check"
 
   timeout_sec        = 5
-  check_interval_sec = 5
+  check_interval_sec = 10
 
-  http_health_check {
+  https_health_check {
     # request_path = "/"
-    port = 80
+    port = 443
   }
 }
+
+// ----------------------------------- HTTP Load Balancer Settings -----------------------------------
+// Global forwarding rule to forward traffic directed at port 80 of created ip address to target our HTTP proxy
+resource "google_compute_global_forwarding_rule" "default-http-forward-rule" {
+  name = "${var.project_name}-port-80"
+  ip_address = google_compute_global_address.default-lb.address
+  port_range = "80"
+  target = google_compute_target_http_proxy.default-http.id
+}
+
+// Target http proxy uses URL map to redirect traffic to https route
+resource "google_compute_target_http_proxy" "default-http" {
+  name = var.project_name
+  url_map = google_compute_url_map.default-http.id
+}
+
+resource "google_compute_url_map" "default-http" {
+  name = "${var.project_name}-http"
+  
+  // no default service here (no backend service, no backend bucket)
+  // only redirection to https
+
+  default_url_redirect {
+    https_redirect = true
+    strip_query    = false
+  }
+}
+
